8fb66383daf12134c26c917f0c48f8733e7b6bac
YannMorgan <yannmorgan56000@gmail.com>
2025-03-19 09:03:07 -0400

    Suppression des fichiers obsolètes

--- src/content.config.ts
+++
@@ -1,8 +0,0 @@
-import { file, glob } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
-
-const data = defineCollection({
-  loader: glob({ pattern: "**/*.json", base: "./src/content/data/" }),
-});
-
-export const collections = { data };
